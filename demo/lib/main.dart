db97d28a4047d37d13ba070bb0488f0125b54acb
haticeyalcinn <htcyalcin38@gmail.com>
2024-05-19 23:10:25 +0300

    odev

---
+++ demo/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //ödev 1
+  int sayi=5;
+  String str = "naber";
+  double sayi2 = 3.5;
+  bool boolean = true;
+  dynamic dyn = 4;
+  print(sayi);
+  print(str);
+  print("Bir boolean değer: $boolean");
+  print("Değiştirelim  :${dyn +4}");
+  print(sayi2);
+  //ödev2
+  int birsayi=5;
+  String bir_str = "naber";
+  double BirSayi =3.5;
+  print("birsayi=$birsayi ,bir_str = $bir_str ,  BirSayi= $BirSayi");
+
+  //ödev3
+
+  String adi ="Hatice";
+  String soyadi ="Yalçın";
+  int yas=23;
+  bool resit = true;
+  print("$adi $soyadi $yas $resit");
+
+
+}
